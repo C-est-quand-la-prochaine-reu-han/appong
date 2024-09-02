ddd384ffb4205ed00519ff51ae9cd57a015fc512
kcouchy <kiri.couchman@gmail.com>
2024-09-02 18:22:02 +0200

    modifications to model: match.tournament_id is now a foreignkey, null=True for all foreignkeys | user_views.create() implemented

--- mysite/appong/models.py
+++ mysite/appong/models.py
@@ -9,19 +9,19 @@ class UserProfile(models.Model):
 	friends_pending = models.ManyToManyField(blank=True, to="UserProfile", related_name="friend_pend")
 
 class Match(models.Model):
-	tournament_id = models.IntegerField(default=0)
+	tournament_id = models.ForeignKey(null=True, blank=True, to="Tournament", on_delete=models.CASCADE, related_name="tournament")
 	player1 = models.ForeignKey(to=UserProfile, on_delete=models.CASCADE, related_name="player1")
 	player2 = models.ForeignKey(to=UserProfile, on_delete=models.CASCADE, related_name="player2")
 	match_start_time = models.DateTimeField(auto_now_add=True)
 	match_end_time = models.DateTimeField(auto_now=True)
-	player1_hit_nb = models.IntegerField(default=0)
-	player2_hit_nb = models.IntegerField(default=0)
-	player1_perfect_hit_nb = models.IntegerField(default=0)
-	player2_perfect_hit_nb = models.IntegerField(default=0)
-	player1_score = models.IntegerField(default=0)
-	player2_score = models.IntegerField(default=0)
-	ball_max_speed = models.IntegerField(default=0)
-	match_status = models.IntegerField(default=0)
+	player1_hit_nb = models.PositiveIntegerField(default=0)
+	player2_hit_nb = models.PositiveIntegerField(default=0)
+	player1_perfect_hit_nb = models.PositiveIntegerField(default=0)
+	player2_perfect_hit_nb = models.PositiveIntegerField(default=0)
+	player1_score = models.PositiveIntegerField(default=0)
+	player2_score = models.PositiveIntegerField(default=0)
+	ball_max_speed = models.PositiveIntegerField(default=0)
+	match_status = models.PositiveIntegerField(default=0)
 
 class Tournament(models.Model):
 	tourn_start_time = models.DateTimeField(auto_now_add=True)
--- mysite/appong/views/user_views.py
+++ mysite/appong/views/user_views.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers, viewsets, permissions
-from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.viewsets import ReadOnlyModelViewSet, ModelViewSet
 from rest_framework.decorators import action
 
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from ..models import UserProfile
+
+import datetime
 
 # Serializers define the API representation.
 
@@ -22,16 +24,11 @@ class UserProfileSerializer(serializers.ModelSerializer):
 		model = UserProfile
 		fields = ['pk', 'user', 'user_nick', 'avatar']
 
-
 ## ViewSets define the view behavior.
 #class UserViewSet(viewsets.ModelViewSet):
 #	queryset = UserProfile.objects.all().order_by('-user__date_joined')
 #	serializer_class = UserProfileSerializer
 #	permission_classes = [permissions.IsAuthenticated]#only logged in users can see data
-
-
-def create(self, data):
-	return UserProfile.objects.create(**data)
 
 def update(self, instance, data):
 	instance.user_nick = data.get('user_nick', instance.user_nick)
@@ -39,8 +36,12 @@ def update(self, instance, data):
 	instance.save()
 	return instance
 
+# def create(self, data):
+# 	return UserProfile.objects.create(**data)
+
 #model class-based view to only get (list() and retrieve())
-class UserProfileViewSet(ReadOnlyModelViewSet):
+# class UserProfileViewSet(ReadOnlyModelViewSet):
+class UserProfileViewSet(ModelViewSet):
 	serializer_class = UserProfileSerializer
 	queryset = UserProfile.objects.all().order_by('-user__date_joined')
 	permission_classes = [permissions.IsAuthenticated]#only logged in users can see data
@@ -55,3 +56,24 @@ class UserProfileViewSet(ReadOnlyModelViewSet):
 		instance.save()
 		#UserProfile.objects.get(pk=pk).delete()
 		return HttpResponse("deleted %s" % pk)
+
+	def create(self, request, *args, **kwargs):
+		new_user = User()
+		new_user.username = request.POST.get("user_nick")
+		new_user.date_joined = datetime.datetime.now()
+		new_user.save()
+		new_userprofile = UserProfile()
+		new_userprofile.user = new_user
+		new_userprofile.user_nick = request.POST.get("user_nick")
+		if request.POST.get("avatar") != '':
+			new_userprofile.avatar = request.POST.get("avatar")
+		new_userprofile.save()
+		return HttpResponse("created user %s" % new_userprofile.user_nick)
+	
+	def update(self, request, pk, *args, **kwargs):
+		pass
+	# FIRST make new users and admin
+		# print(pk) to get user from data Base
+		# request.POST to get the data to change the user
+		# then save
+		# gonna have to read out user id from kwargs
