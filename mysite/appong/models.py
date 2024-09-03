611456dc3b9c34275842e8b33cd6ef6605df3e78
kcouchy <kiri.couchman@gmail.com>
2024-09-03 14:49:14 +0200

    change model: userprofile to user is now a foreignkey | UserProfileViewSet.update is implemente

--- mysite/appong/models.py
+++ mysite/appong/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
-	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	user_nick = models.CharField(max_length=30)
 	avatar = models.ImageField(default="default_avatar.jpg")
 	friends_confirmed = models.ManyToManyField(blank=True, to="UserProfile", related_name="friend_conf")
--- mysite/appong/views/user_views.py
+++ mysite/appong/views/user_views.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers, viewsets, permissions
-from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.viewsets import ReadOnlyModelViewSet, ModelViewSet
 from rest_framework.decorators import action
 
 from django.http import HttpResponse
@@ -7,13 +7,10 @@ from django.contrib.auth.models import User
 from ..models import UserProfile
 
 # Serializers define the API representation.
-
-#default user class attributes
 class UserSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = User
 		fields = ['date_joined']
-
 
 class UserProfileSerializer(serializers.ModelSerializer):
 	user = UserSerializer(required=True)
@@ -22,25 +19,7 @@ class UserProfileSerializer(serializers.ModelSerializer):
 		model = UserProfile
 		fields = ['pk', 'user', 'user_nick', 'avatar']
 
-
-## ViewSets define the view behavior.
-#class UserViewSet(viewsets.ModelViewSet):
-#	queryset = UserProfile.objects.all().order_by('-user__date_joined')
-#	serializer_class = UserProfileSerializer
-#	permission_classes = [permissions.IsAuthenticated]#only logged in users can see data
-
-
-def create(self, data):
-	return UserProfile.objects.create(**data)
-
-def update(self, instance, data):
-	instance.user_nick = data.get('user_nick', instance.user_nick)
-	instance.avatar = data.get('avatar', instance.avatar)
-	instance.save()
-	return instance
-
-#model class-based view to only get (list() and retrieve())
-class UserProfileViewSet(ReadOnlyModelViewSet):
+class UserProfileViewSet(ModelViewSet):
 	serializer_class = UserProfileSerializer
 	queryset = UserProfile.objects.all().order_by('-user__date_joined')
 	permission_classes = [permissions.IsAuthenticated]#only logged in users can see data
@@ -54,4 +33,35 @@ class UserProfileViewSet(ReadOnlyModelViewSet):
 		instance.avatar = "default_avatar.jpg"
 		instance.save()
 		#UserProfile.objects.get(pk=pk).delete()
-		return HttpResponse("deleted %s" % pk)
+		return HttpResponse("deleted user pk=%s" % pk)
+
+	def create(self, request, *args, **kwargs):
+		new_user = User()
+		new_user.username = request.POST.get("user_nick")
+		new_user.date_joined = datetime.datetime.now()
+		new_user.save()
+
+		new_userprofile = UserProfile()
+		new_userprofile.user = new_user
+		new_userprofile.user_nick = request.POST.get("user_nick")
+		if request.POST.get("avatar") != '':
+			new_userprofile.avatar = request.POST.get("avatar")
+		new_userprofile.save()
+
+		return HttpResponse("created user %s" % new_userprofile.user_nick)
+	
+	def update(self, request, pk, *args, **kwargs):
+		if UserProfile.objects.get(pk=pk).user_nick != request.POST.get("user_nick") and User.objects.filter(username=request.POST.get("user_nick")):
+			return HttpResponse("User nick '%s' is not available" % request.POST.get("user_nick"))
+		update_user = User.objects.get(username=UserProfile.objects.get(pk=pk).user_nick)
+		update_user.username = request.POST.get("user_nick")
+		update_user.save()
+
+		update_userprofile = UserProfile.objects.get(pk=pk)
+		update_userprofile.user_nick = request.POST.get("user_nick")
+		if "avatar" in request.FILES:
+			# check somehow the file is valid
+			update_userprofile.avatar = request.FILES["avatar"]
+		update_userprofile.save()
+
+		return HttpResponse("updated user pk=%s" % update_userprofile.pk)
