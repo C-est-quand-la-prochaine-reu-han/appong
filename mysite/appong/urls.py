8e738a81a5b3ed1f3b652f5b0cf7ba2e6836c447
Arthur Boyreau <a.boyreau@proton.me>
2024-09-12 15:18:08 +0200

    basic drf authentication configuration

--- mysite/appong/urls.py
+++ mysite/appong/urls.py
@@ -1,18 +1,20 @@
 from django.urls import path, include
 from . import views
 
+from rest_framework.authtoken import views as drfviews
+
 from rest_framework import routers
 
 app_name = "appong"
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
-# router.register(r'login', views.UserProfileViewSet)
-router.register(r'user', views.UserProfileViewSet)
-router.register(r'match', views.MatchViewSet)
-router.register(r'tournament', views.TournamentViewSet)
+# router.register(r"login", views.UserProfileViewSet)
+router.register(r"user", views.UserProfileViewSet)
+router.register(r"match", views.MatchViewSet)
+router.register(r"tournament", views.TournamentViewSet)
 
 urlpatterns = [
 	path("api/", include(router.urls)),
-	path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+	path("api-auth/", drfviews.obtain_auth_token)
 ]
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'appong.apps.AppongConfig',
     'corsheaders',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
