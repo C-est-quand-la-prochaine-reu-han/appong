380b28a349d7791032faaf1dab38d593ed060c8d
kcouchy <kiri.couchman@gmail.com>
2024-10-14 17:32:12 +0200

    update last_access in db implemented

--- mysite/appong/middlewares.py
+++ mysite/appong/middlewares.py
@@ -1,24 +1,23 @@
 from datetime import datetime
+from django.utils import timezone
 from .models import UserProfile
+from django.utils.timezone import make_aware
 
 class UpdateLastAccessMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
-        # One-time configuration and initialization.
+
 
     def __call__(self, request):
-        # Code to be executed for each request before
-        # the view (and later middleware) are called.
-        try:
-            u = UserProfile.objects.get(user__pk=request.user.pk)
-            u.last_access = datetime.now()
-            u.save()
-        except:
-            pass
 
         response = self.get_response(request)
 
-        # Code to be executed for each request/response after
-        # the view is called.
+        try:
+            u = UserProfile.objects.get(user__pk=request.user.pk)
+            aware_datetime = make_aware(datetime.now())
+            u.last_access = datetime.now(tz=aware_datetime.tzinfo)
+            u.save()
+        except Exception as e:
+            pass
 
         return response
